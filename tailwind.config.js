a9b079213fab83ee8ce7f852fa235234926b6366
Alex Maingi <alexnthuku13718@gmail.com>
2025-04-09 11:39:31 +0300

    other components added

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export const content = [
+  "./src/**/*.{html,js,jsx,ts,tsx,vue}", // Adjust based on your project
+  "./public/index.html", // If using HTML directly
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
+  
